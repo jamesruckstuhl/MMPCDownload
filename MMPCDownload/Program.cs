1c48707165aba38eb22ba6a5d4e46d3fcde7fe35
jamesruckstuhl <jamesruckstuhl@gmail.com>
2015-11-17 15:58:52 -0500

    Revert "Revert "Initial Commit""
    This reverts commit 6e58d0235436e7f47ca3a510b5be172d9d79b529.

---
+++ MMPCDownload/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+
+namespace MMPCDownload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            parseAndDownload(getSourceCode());
+        }
+
+        public static string getSourceCode()
+        {
+            //Get HTML code of web page
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://billburr.libsyn.com");
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            String sourceCode = sr.ReadToEnd();
+            sr.Close();
+            resp.Close();
+            return sourceCode;             
+        }
+
+        public static void parseAndDownload(string sourceCode)
+        {
+            //Get the link within the HTML of the page
+            int startIndex = sourceCode.IndexOf("\"postTitle\"");
+            sourceCode = sourceCode.Substring(startIndex, sourceCode.Length - startIndex);
+            startIndex = sourceCode.IndexOf("<A HREF=") + 9;
+            int endIndex = sourceCode.IndexOf(".mp3",startIndex);
+            string link = sourceCode.Substring(startIndex + 13, endIndex - startIndex - 13) + ".mp3";
+
+            //Establish a connection, download the file, and archive it based on year and month
+            WebClient wc = new WebClient();
+            Uri realLink = new Uri(link);
+            string fileName = link.Substring(35);
+            string filePath = "C:\\Users\\James\\Desktop\\Podcasts\\Monday_Morning_Podcast_Bill_Burr\\" + DateTime.Today.Year + "\\" + DateTime.Today.Month + "\\" + fileName;
+            (new FileInfo(filePath)).Directory.Create();
+            wc.DownloadFileAsync(realLink,filePath);
+        }
+    }
+}
